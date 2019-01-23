3df7bf614e027d98e3d77d558e008b56db439107
blithe-database-2858 <amavelamala@gmail.com>
2019-01-23 00:58:02 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,37 @@
 function basicTeenager(age) {
-
+ if (age >= 13 && age <= 19){
+   console.log("You are a teenager!")
+ }
 }
 
 function teenager(age) {
-
+  if (age >= 13 && age <= 19){
+    console.log("You are a teenager!")
+  } else {
+    console.log("You are not a teenager")
+  }
 }
 
 function ageChecker(age) {
-
+  if (age >= 13 && age <= 19){
+    console.log("You are a teenager!")
+  } else if (age <= 12) {
+    console.log("You are a kid")
+  } else {
+    console.log("You are a grownup")
+  }
 }
 
 function ternaryTeenager(age) {
-
+  return (age >= 13 && age <= 19) ? 
+  "You are a teenager" : 
+  "You are not a teenager"
 }
 
 function switchAge(age) {
-
+  case 13, 14, 15, 16, 17, 18, 19:
+    console.log("You are a teenager");
+    break;
+  default:
+  console.log("You havean age");
 }
